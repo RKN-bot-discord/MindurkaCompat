f82a99eeafcedc59078a8094fa3601c7c055dd9d
buj <buj351@outlook.com>
2025-02-27 00:52:14 +0700

    Added Forts balance changes

--- src/main/java/mindurka/Main.java
+++ src/main/java/mindurka/Main.java
@@ -1,10 +1,16 @@
 package mindurka;
 
+import arc.Events;
+import mindustry.game.EventType;
 import mindustry.mod.Plugin;
 
 public class Main extends Plugin {
     @Override
     public void init() {
+        Events.on(EventType.WorldLoadEndEvent.class, event -> {
+            Stats.updateStats();
+        });
+
         Injects.load();
     }
 }

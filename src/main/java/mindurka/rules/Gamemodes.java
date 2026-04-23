26317087f483828a294238bfbc2e0e98779772fb
buj <buj351@outlook.com>
2026-04-24 01:00:03 +0700

    forgor to mark attack as a vanilla gamemode

--- src/main/java/mindurka/rules/Gamemodes.java
+++ src/main/java/mindurka/rules/Gamemodes.java
@@ -3,7 +3,7 @@ package mindurka.rules;
 public class Gamemodes {
     private Gamemodes() {}
 
-    public static final Gamemode attack = new BlankGamemode("attack");
+    public static final Gamemode attack = new BlankGamemode("attack") {{ vanillaGamemode = true; }};
     public static final Gamemode forts = new Forts();
     public static final Gamemode hexed = new BlankGamemode("hexed-pvp") {{ visible = false; }};
     public static final Gamemode hub = new Hub();
